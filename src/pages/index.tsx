aba61a1e9b10a23135959749bb973eddb3b51ff6
Paulo Robinson <paulo.giaciani@gmail.com>
2022-01-19 14:01:42 -0300

    Completed Header

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { Flex, Heading } from '@chakra-ui/react';
+import { Flex, Heading, Image, Text } from '@chakra-ui/react';
 import type { NextPage } from 'next';
 import Head from 'next/head';
 import Header from './components/Header';
@@ -17,15 +17,43 @@ const Home: NextPage = () => {
           w="100%"
           h="80"
           backgroundSize="cover"
+          px={{ base: '10', md: '10', lg: '32' }}
         >
-          <Flex align="center" w="50%" p="10">
-            <Heading>
+          <Flex
+            align="center"
+            w="100%"
+            direction="column"
+            justify="center"
+            alignItems="flex-start"
+          >
+            <Heading
+              color="gray.200"
+              fontWeight="medium"
+              fontSize={{ base: '26', md: '30', lg: '30', xl: '36' }}
+              lineHeight="1.4"
+              paddingBottom="4"
+            >
               5 Continentes, <br /> infinitas possibilidades.
             </Heading>
+            <Text color="gray.300" fontSize="18">
+              Chegou a hora de tirar do papel a viagem que você sempre sonhou.
+            </Text>
           </Flex>
 
-          <Flex align="center" w="50%" p="10">
-            <h1>Imagem</h1>
+          <Flex
+            align="center"
+            w="100%"
+            position="relative"
+            display={{ sm: 'none', lg: 'flex' }}
+          >
+            <Image
+              src="/images/Airplane.svg"
+              alt="worldtrip"
+              position="absolute"
+              right={0}
+              bottom={-6}
+              h={{ lg: '240', xl: '300' }}
+            />
           </Flex>
         </Flex>
       </Flex>
