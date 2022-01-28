62372f191c0957ceffdbd7e7c1748983214137fe
Paulo Robinson <paulo.giaciani@gmail.com>
2022-01-27 21:52:17 -0300

    Add Link for Header

--- src/pages/components/Header.tsx
+++ src/pages/components/Header.tsx
@@ -1,10 +1,15 @@
 import { Flex, Image } from '@chakra-ui/react';
+import Link from 'next/link';
 
 const Header = () => {
   return (
     <Flex w="100%" direction="column">
       <Flex justify="center" align="center" p="6">
-        <Image src="/images/Logo.svg" alt="worldtrip" h="10" />
+        <Link href="/" passHref>
+          <a>
+            <Image src="/images/Logo.svg" alt="worldtrip" h="10" />
+          </a>
+        </Link>
       </Flex>
     </Flex>
   );
